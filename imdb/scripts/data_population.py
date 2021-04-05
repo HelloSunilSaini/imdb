7b0c226d1c36665973ba31ecca52dcc028c4f6d0
Sunil Saini <sunil.saini@agrostar.in>
2021-04-05 16:48:00 +0530

    data script added

---
+++ imdb/scripts/data_population.py
@@ -0,0 +1,8 @@
+import json
+from imdb.dao.movie import create_movie
+
+if __name__=="__main__":
+    with open('imdb_test_data.json') as f:
+        data = json.load(f)
+    for movie in data:
+        create_movie(movie, None)
